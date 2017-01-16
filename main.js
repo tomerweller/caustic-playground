3e515731be726318d3c59a60292f26f7610f99f3
Tomer Weller <tomer.weller@gmail.com>
2017-01-16 14:15:59 -0500

    progress

--- main.js
+++ main.js
@@ -6,62 +6,62 @@ const loader = new THREE.ObjectLoader();
 let camera, renderer, stats, scene, ball, floor, light, ballGeometry, floorGeometry;
 
 const initScene = loadedScene => {
-    scene = loadedScene;
-    ball = scene.getObjectByName("Ball");
-    ball.material.side = THREE.DoubleSide;
-    ballGeometry = ball.geometry;
+  scene = loadedScene;
+  ball = scene.getObjectByName("Ball");
+  ball.material.side = THREE.DoubleSide;
+  ballGeometry = ball.geometry;
 
-    floor = scene.getObjectByName("Floor");
-    floorGeometry = floor.geometry;
-    floor.material.vertexColors = THREE.FaceColors;
-    // floor.material.side = THREE.DoubleSide;
+  floor = scene.getObjectByName("Floor");
+  floorGeometry = floor.geometry;
+  floor.material.vertexColors = THREE.FaceColors;
+  // floor.material.side = THREE.DoubleSide;
 
-    light = scene.getObjectByName("PointLight");
-    console.log("scene loading...");
-    console.log(scene);
+  light = scene.getObjectByName("PointLight");
+  console.log("scene loading...");
+  console.log(scene);
 
-    const container = document.createElement( 'div' );
-    document.body.appendChild( container );
+  const container = document.createElement( 'div' );
+  document.body.appendChild( container );
 
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.set( 0.0, 10, 10 * 3.5 );
+  camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.set( 0.0, 10, 10 * 3.5 );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.shadowMap.enabled = true;
-    container.appendChild(renderer.domElement);
-    renderer.gammaInput = true;
-    renderer.gammaOutput = true;
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.shadowMap.enabled = true;
+  container.appendChild(renderer.domElement);
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
 
 
-    stats = new Stats();
-    container.appendChild( stats.dom );
-    controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.target.set( 0, 0, 0 );
-    controls.update();
-    initSettings();
-    animate();
+  stats = new Stats();
+  container.appendChild( stats.dom );
+  controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls.target.set( 0, 0, 0 );
+  controls.update();
+  initSettings();
+  animate();
 };
 
 const onWindowResize = () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setSize( width, height );
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize( width, height );
 };
 
 const animate = () => {
-    requestAnimationFrame( animate );
-    stats.begin();
-    render();
-    stats.end();
+  requestAnimationFrame( animate );
+  stats.begin();
+  render();
+  stats.end();
 };
 
 const render = () => {
-    light.castShadow = settings.lightPointCastShadows;
-    renderer.render( scene, camera );
+  light.castShadow = settings.lightPointCastShadows;
+  renderer.render( scene, camera );
 };
 
 const RINDEX_AIR = 1;
@@ -73,89 +73,96 @@ const deg = (rad) => rad*(180/Math.PI);
 const log = console.log;
 
 const projectFacesBallBall = (f) => {
-    //face vertices, global
+  //face vertices, global
 
-    const vertices = [f.a, f.b, f.c].map(v => {
-        const position = ballGeometry.vertices[v].clone().add(ball.position);
-        return {
-            position,
-            inLightDirection: position.clone().sub(light.position).normalize()
-        }
-    });
+  const vertices = f.vertexPositions.map(position => ({
+    position,
+    inLightDirection: position.clone().sub(light.position).normalize()
+  }));
 
-    const isFaceDirect = vertices.reduce((isTrue, v) => {
-        const isecs = raycast(light.position, v.inLightDirection, ball);
-        return isTrue && isecs.length>0 && isecs[0].point.distanceTo(v.position)<EQUALITY_THRESHEOLD;
-    }, true);
+  const isFaceDirect = vertices.reduce((isTrue, v) => {
+    const isecs = raycast(light.position, v.inLightDirection, ball);
+    return isTrue && isecs.length>0 && isecs[0].point.distanceTo(v.position)<EQUALITY_THRESHEOLD;
+  }, true);
 
-    let faceProjections = [];
+  let faceProjections = [];
 
-    if (isFaceDirect) {
-        faceProjections = vertices.reduce( (prev, v) => {
-            const ray = refract(f.normal, v.inLightDirection.clone().multiplyScalar(-1), RINDEX_AIR, RINDEX_GLASS);
-            const isecs = raycast(v.position, ray, ball);
-            //we might only care about the furthest but taking all for now
-            const filteredIsecs = isecs.filter(isec => isec.distance>EQUALITY_THRESHEOLD);
-            return prev.concat(filteredIsecs.map(isec => ({face: isec.face, ray})));
-        }, []);
-    }
+  if (isFaceDirect) {
+    faceProjections = vertices.reduce( (prev, v) => {
+      const ray = refract(f.normal, v.inLightDirection.clone().multiplyScalar(-1), RINDEX_AIR, RINDEX_GLASS);
+      const isecs = raycast(v.position, ray, ball);
+      //we might only care about the furthest but taking all for now
+      const filteredIsecs = isecs.filter(isec => isec.distance>EQUALITY_THRESHEOLD);
+      return prev.concat(filteredIsecs.map(isec => ({face: isec.face, ray})));
+    }, []);
+  }
 
-    return faceProjections;
+  return faceProjections;
 };
 
 const testCalcFace = () => projectFacesBallBall(ballGeometry.faces[0]);
 
 const projectFacesBallWall = pf => {
 
-    const vertices = [pf.face.a, pf.face.b, pf.face.c]
-        .map(v => ballGeometry.vertices[v].clone().add(ball.position));
+  const vertices = [pf.face.a, pf.face.b, pf.face.c]
+    .map(v => new THREE.Vector3().fromBufferAttribute( ball.geometry.attributes.position, v).add(ball.position));
 
-    return vertices.reduce( (prev, v) => {
-        const ray = refract(pf.face.normal.clone().multiplyScalar(-1), pf.ray.clone().multiplyScalar(-1), RINDEX_GLASS, RINDEX_AIR);
-        const isecs = raycast(v, ray, floor);
-        //we might only care about the furthest but taking all for now
-        return prev.concat(isecs);
-    }, []);
+  return vertices.reduce( (prev, v) => {
+    const ray = refract(pf.face.normal.clone().multiplyScalar(-1), pf.ray.clone().multiplyScalar(-1), RINDEX_GLASS, RINDEX_AIR);
+    const isecs = raycast(v, ray, floor);
+    //we might only care about the furthest but taking all for now
+    return prev.concat(isecs);
+  }, []);
 
 };
 
 const getCausticMap = () => {
-    console.log("0", performance.now());
+  const start = performance.now();
+  console.log("0", performance.now()-start);
 
-    const ballBallFaces = ballGeometry.faces.reduce((prev, f) => {
-        return prev.concat(projectFacesBallBall(f));
-    }, []);
+  let ballBallFaces = [];
+  for (face of faceIterator(ball.geometry, ball.position)) {
+    for (projectedFace of projectFacesBallBall(face)) {
+      ballBallFaces.push(projectedFace);
+    }
+  }
 
-    console.log("1", performance.now());
-    const ballFloorFaces =  ballBallFaces.reduce((prev, projectedFace) => {
-        return prev.concat(projectFacesBallWall(projectedFace));
-    }, []);
+  console.log("1", performance.now());
+  const ballFloorFaces =  ballBallFaces.reduce((prev, projectedFace) => {
+    return prev.concat(projectFacesBallWall(projectedFace));
+  }, []);
 
-    console.log("2", performance.now());
-
-    const causticMap = {};
-    ballFloorFaces.forEach(f => {
-        if (!(f.faceIndex in causticMap)){
-            causticMap[f.faceIndex] = 0;
-        }
-        causticMap[f.faceIndex]+=1;
-    });
-    console.log("3", performance.now());
-    return causticMap;
+  console.log("2", performance.now()-start);
+  const causticMap = {};
+  ballFloorFaces.forEach(f => {
+    if (!(f.faceIndex in causticMap)){
+      causticMap[f.faceIndex] = 0;
+    }
+    causticMap[f.faceIndex]+=1;
+  });
+  console.log("3", performance.now()-start);
+  return causticMap;
 };
 
 const applyCaustics = (causticMap, fnGetter) => {
-    const max = Object.keys(causticMap).reduce((currMax,i) => {
-        return causticMap[i]>currMax? causticMap[i]: currMax;
-    }, Number.MIN_SAFE_INTEGER);
-    console.log("max is", max);
-    const fn = fnGetter(max);
+  const max = Object.keys(causticMap).reduce((currMax,i) => {
+    return causticMap[i]>currMax? causticMap[i]: currMax;
+  }, Number.MIN_SAFE_INTEGER);
 
-    floorGeometry.faces.forEach((face,i) => {
-        const val = i in causticMap ? fn(causticMap[i]) : 0;
-        face.color.setRGB(val,val,val);
-    });
-    floorGeometry.colorsNeedUpdate = true;
+  console.log("max is", max);
+  const fn = fnGetter(max);
+
+  // floor.geometry.attributes.color = new BufferAttribute();
+
+  const color = new Float32Array(floor.geometry.index.count);
+
+  for(face of faceIterator(floor.geometry, floor.position)) {
+    const val = face.index in causticMap ? fn(causticMap[face.index]) : 0;
+    [0,1,2].map(i => color[3*face.index+i] = val)
+  }
+  floor.geometry.addAttribute('color', new THREE.BufferAttribute(color, 3) );
+  floor.geometry.attributes.color.needsUpdate = true;
+  // floorGeometry.colorsNeedUpdate = true;
 };
 
 const getArctanFn = max => x => Math.atan(50*(x/max))/(Math.PI/2);
@@ -163,27 +170,27 @@ const getLinearFn = max => x => x/max;
 const getQuadraticFn = max => x => Math.pow(x/max,2);
 
 const testCaustics = () => {
-    const map = getCausticMap();
-    applyCaustics(map, getArctanFn);
+  const map = getCausticMap();
+  applyCaustics(map, getArctanFn);
 };
 
 const colorGeometry = geometry => {
-    geometry.faces.forEach(face => face.color.setRGB(Math.random(),Math.random(),Math.random()));
-    floor.material.needsUpdate = true;
-    geometry.colorsNeedUpdate = true;
+  geometry.faces.forEach(face => face.color.setRGB(Math.random(),Math.random(),Math.random()));
+  floor.material.needsUpdate = true;
+  geometry.colorsNeedUpdate = true;
 };
 
 loader.load( "plane-scene.json", initScene);
 window.addEventListener( 'resize', onWindowResize, false );
 
 const settings = {
-    lightPointCastShadows: true,
-    testCaustics: testCaustics
+  lightPointCastShadows: true,
+  testCaustics: testCaustics
 };
 
 const initSettings = () => {
-    const gui = new dat.GUI();
-    gui.add(settings, 'lightPointCastShadows');
-    gui.add(settings, 'testCaustics');
+  const gui = new dat.GUI();
+  gui.add(settings, 'lightPointCastShadows');
+  gui.add(settings, 'testCaustics');
 };
 
--- util.js
+++ util.js
@@ -1,69 +1,53 @@
 const perf = (fn) => {
-    const t0 = performance.now();
-    const retval = fn();
-    const t1 = performance.now();
-    console.log("Call to fn took " + (t1 - t0)/1000 + " seconds.");
-    return retval;
+  const t0 = performance.now();
+  const retval = fn();
+  const t1 = performance.now();
+  console.log("Call to fn took " + (t1 - t0)/1000 + " seconds.");
+  return retval;
 };
 
 const raycaster = new THREE.Raycaster();
 const raycast = (origin, direction, mesh) => {
-    raycaster.set(origin, direction);
-    return raycaster.intersectObject(mesh);
+  raycaster.set(origin, direction);
+  return raycaster.intersectObject(mesh);
 };
 
 const refract = (surfaceNormal, inRay, inIOR, outIOR) => {
-    const theta1 = surfaceNormal.clone().angleTo(inRay); //+
-    // console.log("theta1 is", deg(theta1));
-    const refractAngle = Math.asin((inIOR/outIOR)*Math.sin(theta1)); //+
-    // console.log("refract angle", deg(refractAngle));
-    const reverseSurfaceNormal = surfaceNormal.clone().multiplyScalar(-1);
-    // console.log("reverse surface normal", reverseSurfaceNormal);
-    const rotationAxis = surfaceNormal.clone().cross(inRay).normalize();
-    // console.log("rotation axis is", rotationAxis);
-    const out = reverseSurfaceNormal.clone().applyAxisAngle(rotationAxis, refractAngle);
-    // console.log("out", out);
-    return out;
+  const theta1 = surfaceNormal.clone().angleTo(inRay); //+
+  // console.log("theta1 is", deg(theta1));
+  const refractAngle = Math.asin((inIOR/outIOR)*Math.sin(theta1)); //+
+  // console.log("refract angle", deg(refractAngle));
+  const reverseSurfaceNormal = surfaceNormal.clone().multiplyScalar(-1);
+  // console.log("reverse surface normal", reverseSurfaceNormal);
+  const rotationAxis = surfaceNormal.clone().cross(inRay).normalize();
+  // console.log("rotation axis is", rotationAxis);
+  const out = reverseSurfaceNormal.clone().applyAxisAngle(rotationAxis, refractAngle);
+  // console.log("out", out);
+  return out;
 };
 
-const getVertex = (geometry, index) => {
-    const position = new THREE.Vector3();
-    const normal = new THREE.Vector3();
+const faceInBufferedGeometry = (geometry, faceIndex, offset) => {
+  const vertexPositions = [0,1,2]
+    .map(i => geometry.index.array[3*faceIndex+i])
+    .map(vertexIndex =>
+      new THREE.Vector3().fromBufferAttribute( geometry.attributes.position, vertexIndex).add(offset));
+  const  normal = new THREE.Vector3().crossVectors(
+    new THREE.Vector3().subVectors(vertexPositions[1], vertexPositions[0]),
+    new THREE.Vector3().subVectors(vertexPositions[2], vertexPositions[0])
+  ).normalize();
+  return {
+    index : faceIndex,
+    normal,
+    vertexPositions
+  };
+};
 
-    return {
-        position: position.fromBufferAttribute( geometry.attributes.position, index),
-        normal: normal.fromBufferAttribute( geometry.attributes.normal, index)
+const faceIterator = (geometry, offset) => { //for meshes with buffered geometry
+  const iterable = {};
+  iterable[Symbol.iterator] = function* () {
+    for (let i=0; i<(geometry.index.count/3); i++) {
+      yield faceInBufferedGeometry(geometry, i, offset);
     }
+  };
+  return iterable;
 };
-
-const faceInBufferedGeomtry = (geometry, faceIndex) => {
-    const vertices = [0,1,2].map(i => geometry.index.array[3*faceIndex+i]).map(i => getVertex(geometry, i));
-    const positions = vertices.map(v => v.position);
-    // const faceNormal = vertices.reduce((curr, v) => curr.add(), new THREE.Vector3(0,0,0)).normalize();
-    var faceNormal = new THREE.Vector3().crossVectors(
-        new THREE.Vector3().subVectors( b, a ),
-        new THREE.Vector3().subVectors( c, a )
-    ).normalize();
-
-    return {
-        normal: faceNormal,
-        positions
-    };
-};
-
-// const faceIterator = mesh => { //for meshes with buffered geometry
-//     const iterable = {};
-//     iterable[Symbol.iterator] = function* () {
-//         const vertexCount = mesh.geometry.attributes.normal.count;
-//         const normalArr = mesh.geometry.attributes.normal.array;
-//         const positionArr = mesh.geometry.attributes.position.array;
-//         for (let i=0; i<vertexCount; i++) {
-//             const projectedI = i*3;
-//             yield {
-//                 normal: vector3FromArr(normalArr.subarray(projectedI,projectedI+3)),
-//                 position: vector3FromArr(positionArr.subarray(projectedI,projectedI+3)),
-//             }
-//         }
-//     };
-//     return iterable;
-// };
