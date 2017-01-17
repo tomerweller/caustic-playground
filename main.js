f712aecace69b0a33c8b86853a89611672df531c
Tomer Weller <tomer.weller@gmail.com>
2017-01-16 20:34:44 -0500

    some settings

--- main.js
+++ main.js
@@ -3,66 +3,71 @@
  */
 
 const loader = new THREE.ObjectLoader();
-let camera, renderer, stats, scene, ball, floor, light, ballGeometry, floorGeometry;
+let camera, renderer, stats, scene, ball, floor, light, ballGeometry, floorGeometry, ambient;
 
 const initScene = loadedScene => {
-    scene = loadedScene;
-    ball = scene.getObjectByName("Ball");
-    ball.material.side = THREE.DoubleSide;
-    ballGeometry = ball.geometry;
+  scene = loadedScene;
+  ball = scene.getObjectByName("Ball");
+  ball.material.side = THREE.DoubleSide;
+  ballGeometry = ball.geometry;
 
-    floor = scene.getObjectByName("Floor");
-    floorGeometry = floor.geometry;
-    floor.material.vertexColors = THREE.VertexColors;
-    // floor.material.side = THREE.DoubleSide;
+  floor = scene.getObjectByName("Floor");
+  floorGeometry = floor.geometry;
+  floor.material.vertexColors = THREE.VertexColors;
+  // floor.material.side = THREE.DoubleSide;
 
-    light = scene.getObjectByName("PointLight");
-    console.log("scene loading...");
-    console.log(scene);
+  ambient = scene.getObjectByName("AmbientLight");
 
-    const container = document.createElement( 'div' );
-    document.body.appendChild( container );
+  light = scene.getObjectByName("PointLight");
+  console.log("scene loading...");
+  console.log(scene);
 
-    camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
-    camera.position.set( 0.0, 10, 10 * 3.5 );
+  const container = document.createElement( 'div' );
+  document.body.appendChild( container );
 
-    renderer = new THREE.WebGLRenderer();
-    renderer.setPixelRatio( window.devicePixelRatio );
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    renderer.shadowMap.enabled = true;
-    container.appendChild(renderer.domElement);
-    renderer.gammaInput = true;
-    renderer.gammaOutput = true;
+  camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 2000 );
+  camera.position.set( 0.0, 10, 10 * 3.5 );
+
+  renderer = new THREE.WebGLRenderer();
+  renderer.setPixelRatio( window.devicePixelRatio );
+  renderer.setSize( window.innerWidth, window.innerHeight );
+  renderer.shadowMap.enabled = true;
+  container.appendChild(renderer.domElement);
+  renderer.gammaInput = true;
+  renderer.gammaOutput = true;
 
 
-    stats = new Stats();
-    container.appendChild( stats.dom );
-    const controls = new THREE.OrbitControls( camera, renderer.domElement );
-    controls.target.set( 0, 0, 0 );
-    controls.update();
-    initSettings();
-    animate();
+  stats = new Stats();
+  container.appendChild( stats.dom );
+  const controls = new THREE.OrbitControls( camera, renderer.domElement );
+  controls.target.set( 0, 0, 0 );
+  controls.update();
+  initSettings();
+  animate();
 };
 
 const onWindowResize = () => {
-    const width = window.innerWidth;
-    const height = window.innerHeight;
-    camera.aspect = width / height;
-    camera.updateProjectionMatrix();
-    renderer.setSize( width, height );
+  const width = window.innerWidth;
+  const height = window.innerHeight;
+  camera.aspect = width / height;
+  camera.updateProjectionMatrix();
+  renderer.setSize( width, height );
 };
 
 const animate = () => {
-    requestAnimationFrame( animate );
-    stats.begin();
-    render();
-    stats.end();
+  requestAnimationFrame( animate );
+  stats.begin();
+  render();
+  stats.end();
 };
 
 const render = () => {
-    light.castShadow = settings.lightPointCastShadows;
-    ball.material.opacity = settings.refractiveOpacity;
-    renderer.render( scene, camera );
+  light.castShadow = settings.lightPointCastShadows;
+  ball.material.opacity = settings.refractiveOpacity;
+  ambient.intensity = settings.ambientIntensity;
+  const floorRGB = settings.floorColor.map(c => Math.floor(c));
+  floor.material.color = new THREE.Color(`rgb(${floorRGB[0]}, ${floorRGB[1]}, ${floorRGB[2]})`);
+  renderer.render( scene, camera );
 };
 
 const deg = (rad) => rad*(180/Math.PI);
@@ -70,67 +75,70 @@ const deg = (rad) => rad*(180/Math.PI);
 const log = console.log;
 
 const projectFacesBallWall = pf => {
-    const vertices = [pf.face.a, pf.face.b, pf.face.c]
-        .map(v => new THREE.Vector3().fromBufferAttribute( ball.geometry.attributes.position, v).add(ball.position));
+  const vertices = [pf.face.a, pf.face.b, pf.face.c]
+    .map(v => new THREE.Vector3().fromBufferAttribute( ball.geometry.attributes.position, v).add(ball.position));
 
-    return vertices.reduce( (prev, v) => {
-        const ray = refract(pf.face.normal.clone().multiplyScalar(-1), pf.ray.clone().multiplyScalar(-1), RINDEX_GLASS, RINDEX_AIR);
-        const isecs = raycast(v, ray, floor);
-        //we might only care about the furthest but taking all for now
-        return prev.concat(isecs);
-    }, []);
+  return vertices.reduce( (prev, v) => {
+    const ray = refract(pf.face.normal.clone().multiplyScalar(-1), pf.ray.clone().multiplyScalar(-1), RINDEX_GLASS, RINDEX_AIR);
+    const isecs = raycast(v, ray, floor);
+    //we might only care about the furthest but taking all for now
+    return prev.concat(isecs);
+  }, []);
 };
 
 const getMainCausticMap = () => getCausticMap(light.position, ball, floor, RINDEX_AIR, RINDEX_GLASS);
 
 const applyCaustics = (causticMap, fnGetter) => {
-    const max = Object.keys(causticMap).reduce((currMax,i) => {
-        return causticMap[i]>currMax? causticMap[i]: currMax;
-    }, Number.MIN_SAFE_INTEGER);
+  const max = Object.keys(causticMap).reduce((currMax,i) => {
+    return causticMap[i]>currMax? causticMap[i]: currMax;
+  }, Number.MIN_SAFE_INTEGER);
 
-    console.log("max is", max);
-    const fn = fnGetter(max);
+  console.log("max is", max);
+  const fn = fnGetter(max);
 
-    const color = new Float32Array(floor.geometry.attributes.position.count*3);
-    for(face of faceIterator(floor.geometry, floor.matrixWorld)) {
-        const val = face.index in causticMap ? fn(causticMap[face.index]) : 0;
-        [0,1,2]
-            .map(i => floorGeometry.index.array[3*face.index+i])
-            .map(vertexIndex => {
-                [0,1,2].map(j => color[vertexIndex*3+j]= val);
-            })
-    }
+  const color = new Float32Array(floor.geometry.attributes.position.count*3);
+  for(face of faceIterator(floor.geometry, floor.matrixWorld)) {
+    const val = face.index in causticMap ? fn(causticMap[face.index]) : 0;
+    [0,1,2]
+      .map(i => floorGeometry.index.array[3*face.index+i])
+      .map(vertexIndex => {
+        [0,1,2].map(j => color[vertexIndex*3+j]= val);
+      })
+  }
 
-    floor.geometry.addAttribute('color', new THREE.BufferAttribute(color, 3) );
-    floor.geometry.attributes.color.needsUpdate = true;
-    // floorGeometry.colorsNeedUpdate = true;
+  floor.geometry.addAttribute('color', new THREE.BufferAttribute(color, 3) );
+  floor.geometry.attributes.color.needsUpdate = true;
+  // floorGeometry.colorsNeedUpdate = true;
 };
 
 const getArctanFn = max => x => Math.atan(50*(x/max))/(Math.PI/2);
 const getLinearFn = max => x => x/max;
 const getQuadraticFn = max => x => Math.pow(x/max,2);
 
-const testCaustics = () => {
-    window.map = perf(getMainCausticMap);
-
-    //HACK!
-    window.directFaces.forEach(faceIndex => window.map[faceIndex] = faceIndex in window.map? window.map[faceIndex]+1: 0);
-
-    applyCaustics(window.map, getArctanFn);
+const calcCaustics = () => {
+  window.map = perf(getMainCausticMap);
+  //HACK!
+  window.directFaces.forEach(faceIndex => window.map[faceIndex] = faceIndex in window.map? window.map[faceIndex]+1: 0);
+  applyCaustics(window.map, getArctanFn);
 };
 
 loader.load( "scene.json", initScene);
 window.addEventListener( 'resize', onWindowResize, false );
 
 const settings = {
-    refractiveOpacity: 0.5,
-    lightPointCastShadows: false,
-    testCaustics: testCaustics
+  refractiveOpacity: 0.5,
+  lightPointCastShadows: false,
+  calcCaustics: calcCaustics,
+  ambientIntensity: 50,
+  floorColor: [255,180,0]
 };
 
 const initSettings = () => {
-    const gui = new dat.GUI();
-    gui.add(settings, 'lightPointCastShadows');
-    gui.add(settings, 'refractiveOpacity', 0, 1);
-    gui.add(settings, 'testCaustics');
+  const gui = new dat.GUI();
+  // gui.add(settings, 'lightPointCastShadows');
+  gui.add(settings, 'refractiveOpacity', 0, 1);
+  gui.add(settings, 'ambientIntensity', 0, 150);
+  gui.addColor(settings, 'floorColor')
+  gui.add(settings, 'calcCaustics');
+
 };
